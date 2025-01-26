081354229d9012d22872c961819c5e6e27401a7c
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-01-26 21:08:37 +0000

    Header

--- portfolio/components/Header.jsx
+++ portfolio/components/Header.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-
+import Link from "next/link";
+import { Button } from "./ui/button";
 const Header = () => {
-    return <header>headerPrizley</header>;
+    return (
+        <header className="py-8 xl:py-12 text-white bg-pink-50/20">
+            <div className="container mx-auto">
+                {/* logoTipo*/}
+                <Link href="/">
+                    <h1 className="text-4xl font-semibold">
+                        Elvis<span className="text-cyan-600">.</span>
+                    </h1>
+                </Link>
+                {/* Desktop nov */}
+                <Nav />
+            </div>
+        </header>
+    );
 };
 
 export default Header;
