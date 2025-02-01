693220e28efcb91c0c07717ca7006835a3e15c36
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-02-01 15:52:25 +0000

    Stair

--- portfolio/components/Stairs.jsx
+++ portfolio/components/Stairs.jsx
@@ -1,7 +1,46 @@
+import { animate, delay } from "framer-motion";
+import { motion } from "framer-motion";
 import React from "react";
 
+const stairAnimation = {
+    initial: {
+        top: "0%",
+    },
+    animate: {
+        top: "100%",
+    },
+    exit: {
+        top: ["100%", "0%"],
+    },
+};
+
+const reverseIndex = (index) => {
+    const totalSteps = 6;
+    return totalSteps - index - 1;
+};
 const Stairs = () => {
-    return <div>Stairs</div>;
+    return (
+        <>
+            {/*Render 6 motion divs, each representating o step of the stairs*/}
+            {[...Array(6)].map((_, index) => {
+                return (
+                    <motion.div
+                        key={index}
+                        variants={stairAnimation}
+                        initial="initial"
+                        animate="animate"
+                        exit="exit"
+                        transition={{
+                            duration: 0.3,
+                            ease: "easeInOut",
+                            delay: reverseIndex(index) * 0.1,
+                        }}
+                        className="h-full w-full bg-blue-950 relative"
+                    />
+                );
+            })}
+        </>
+    );
 };
 
 export default Stairs;
