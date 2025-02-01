5a31e39fd49a738cbf07cf8ed893b263827bd470
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-02-01 16:55:38 +0000

    Update Stair

--- portfolio/components/StairTransition.jsx
+++ portfolio/components/StairTransition.jsx
@@ -2,6 +2,7 @@
 import { AnimatePresence } from "framer-motion";
 import { usePathname } from "next/navigation";
 import Stairs from "./Stairs";
+import { motion } from "framer-motion";
 
 const StairTransition = () => {
     const pathname = usePathname();
@@ -12,6 +13,19 @@ const StairTransition = () => {
                     <div className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex">
                         <Stairs />
                     </div>
+
+                    <motion.div
+                        className=" h-screen w-screen fixed bg-sky-800 top-0 pointer-events-none"
+                        initial={{ opacity: 1 }}
+                        animate={{
+                            opacity: 0,
+                            transition: {
+                                duration: 0.3,
+                                delay: 0.3,
+                                ease: "easeInOut",
+                            },
+                        }}
+                    />
                 </div>
             </AnimatePresence>
         </>
