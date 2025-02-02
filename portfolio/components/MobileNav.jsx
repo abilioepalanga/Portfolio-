a540a2e92f0e0dbf1399cd247250ca8f87cf2bd1
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-02-02 20:43:29 +0000

    Color Nav Mobile

--- portfolio/components/MobileNav.jsx
+++ portfolio/components/MobileNav.jsx
@@ -54,10 +54,19 @@ const MobileNav = () => {
                     </Link>
                 </div>
                 {/* nav */}
-                <nav>
+                <nav className="flex flex-col justify-center items-center gap-8">
                     {links.map((link, index) => {
                         return (
-                            <Link href={link.path} key={index} className="">
+                            <Link
+                                href={link.path}
+                                key={index}
+                                className={`
+                                    ${
+                                        link.path === pathname &&
+                                        "text-cyan-600 border-b-2 border-cyan-600"
+                                    } 
+                                    text-xl capitalize hover:text-cyan-600 transition-all`}
+                            >
                                 {link.name}
                             </Link>
                         );
