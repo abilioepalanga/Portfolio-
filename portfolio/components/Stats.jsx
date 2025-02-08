98ab05c1b5c7117449e72fabf13b3fe205a67dd5
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-02-08 17:38:55 +0000

    Update Stats

--- portfolio/components/Stats.jsx
+++ portfolio/components/Stats.jsx
@@ -9,7 +9,26 @@ const stats = [
 ];
 
 const Stats = () => {
-    return <div>Stats</div>;
+    return (
+        <section>
+            <div className="container mx-auto">
+                <div>
+                    {stats.map((item, index) => {
+                        return (
+                            <div key={index}>
+                                <CountUp
+                                    end={item.num}
+                                    duration={5}
+                                    delay={2}
+                                    className="text-4xl xl:text-6xl front-extrabold"
+                                />
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Stats;
