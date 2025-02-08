fb81d45d5d205ec288ea38da42abc41b04a8723e
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-02-08 18:51:54 +0000

    Put space between number stats

--- portfolio/components/Stats.jsx
+++ portfolio/components/Stats.jsx
@@ -12,10 +12,14 @@ const Stats = () => {
     return (
         <section>
             <div className="container mx-auto">
-                <div>
+                <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
                     {stats.map((item, index) => {
                         return (
-                            <div key={index}>
+                            <div
+                                className="flex-1 flex gap-4 items-center justify-center justify-center
+                            xl:justify-start"
+                                key={index}
+                            >
                                 <CountUp
                                     end={item.num}
                                     duration={5}
