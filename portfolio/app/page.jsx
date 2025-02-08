9b9f8974b3d02be292e3aec8334072074262fa04
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-02-08 17:12:01 +0000

    Install react count up

--- portfolio/app/page.jsx
+++ portfolio/app/page.jsx
@@ -3,6 +3,7 @@ import { FiDownload } from "react-icons/fi";
 import Download from "@/components/Download";
 import Social from "@/components/Social";
 import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
 
 const Home = () => {
     return (
@@ -48,6 +49,7 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+            <Stats />
         </section>
     );
 };
