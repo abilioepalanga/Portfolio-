b304986385b2a2d306aa823b5eda3585a3e78787
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-02-08 17:49:29 +0000

    Already updating

--- portfolio/components/Stats.jsx
+++ portfolio/components/Stats.jsx
@@ -2,10 +2,10 @@
 import CountUp from "react-countup";
 
 const stats = [
-    { number: 5, text: "Years of Experience" },
-    { number: 10, text: "Projects Completed In The Last Year" },
-    { number: 3, text: "Companies Worked" },
-    { number: 100, text: "Code Commits" },
+    { num: 5, text: "Years of Experience" },
+    { num: 10, text: "Projects Completed In The Last Year" },
+    { num: 3, text: "Companies Worked" },
+    { num: 100, text: "Code Commits" },
 ];
 
 const Stats = () => {
