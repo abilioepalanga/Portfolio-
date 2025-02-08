81f2e9dbab1a096ec09f2b88e0a72c70c9da3796
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-02-08 17:21:10 +0000

    create variable stats

--- portfolio/components/Stats.jsx
+++ portfolio/components/Stats.jsx
@@ -1,4 +1,12 @@
 "use client";
+import CountUp from "react-countup";
+
+const stats = [
+    { number: 5, text: "Years of Experience" },
+    { number: 10, text: "Projects Completed In The Last Year" },
+    { number: 3, text: "Companies Worked" },
+    { number: 100, text: "Code Commits" },
+];
 
 const Stats = () => {
     return <div>Stats</div>;
