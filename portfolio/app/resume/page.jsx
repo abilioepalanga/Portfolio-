f028a301db4c12656c5e96e41b8128514ffc56fd
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-02-11 08:43:33 +0000

    Fixed bug Service Page

--- portfolio/app/resume/page.jsx
+++ portfolio/app/resume/page.jsx
@@ -1,4 +1,12 @@
-import React from "react";
+"use client";
+import {
+    FaHtml5,
+    FaCss3,
+    FaJs,
+    FaReact,
+    FaFigma,
+    FaNodeJs,
+} from "react-icons/fa";
 
 const Resume = () => {
     return <div>resume page</div>;
